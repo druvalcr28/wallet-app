357811c36e8efda91a57d7f54e76add92ccb3ec1
druvalcr28 <bim2016501@iiita.ac.in>
2020-03-30 23:05:50 +0530

    add styles to login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
 import React from 'react';
 // We use 'Link' or 'NavLink' instead of 'a' tag because, 'Link' provide us client side routing
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 export const Header = (props) => (
-    <div>
-        <h1>Expensify</h1>
-        <div>
-            <NavLink to="/dashboard" activeClassName="is-active" exact={true}><p>DashBoard</p></NavLink>
-            <NavLink to="/create" activeClassName="is-active"><p>Create Expense</p></NavLink>
-            <button onClick={() => {
-                props.dispatch(startLogout());
-            }}>Logout</button>
+    <div className="header">
+        <div className="content-container">
+        <Container>
+            <Row>
+                <Col xs={2}>
+                    <Link to="/dashboard" className="header__title" style={{ textDecoration:'none' }}><h1>Wallet</h1></Link>
+                </Col>
+                <Col xs={4}></Col>
+                <Col xs={6}>
+                    <Button className="box-layout__button header__button" onClick={() => {props.dispatch(startLogout());}} varient="flat">Logout</Button>
+                </Col>
+            </Row>
+        </Container>
         </div>
     </div>
 );
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export const LoginPage = (props) => {
     return (
-        <div>
-            <button onClick={() => {
-                props.dispatch(startLogin());
-            }}>Login</button>
+        <div className="box-layout">
+            <Card className="box-layout__card" style={{ width: '18rem' }}>
+                <Card.Header><h3 className="box-layout__title">Wallet App</h3></Card.Header>
+                <Card.Body>
+                    <Card.Title><h5 className="box-layout__subtitle">Sign-In with Google account</h5></Card.Title>
+                    
+                    <div align="center">
+                    <Button className="box-layout__button" onClick={() => {props.dispatch(startLogin());}} variant="flat">Login</Button>
+                    </div>
+                    
+                </Card.Body>
+            </Card>
         </div>
     );
 };
