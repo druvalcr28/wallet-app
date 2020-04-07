7517f009d516ebe62aa8dd2d2fe05f028a1141e5
druvalcr28 <bim2016501@iiita.ac.in>
2020-04-07 11:37:30 +0530

    check heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import * as firebase from 'firebase';
-import { firebaseConfig } from './config';
+import { firebaseConfig } from './configFirebase';
 
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
